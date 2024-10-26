8d5bdc596842c108828d063e549b89b03860d83c
drong24 <daisyrong24@gmail.com>
2024-10-26 15:31:24 -0700

    updated ui

--- TaxiMeter/TaxiMeter/ContentView.swift
+++ TaxiMeter/TaxiMeter/ContentView.swift
@@ -15,19 +15,24 @@ struct ContentView: View {
         
         VStack {
             switch locationDataManager.locationManager.authorizationStatus {
+            case .authorizedWhenInUse:
+                /*
+                Text("Your current location is:")
+                Text("lat: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error")")
+                 Text("lng: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error")")
+                 */
+                Text("Distance Traveled: \(String(format:"%.2f", distanceTraveled))")
+                Text("Price Per Mile: ")
+                Text("Initial fee: ")
+                Text("Total fare: ")
+                Button("Start Location Tracking", action: {
+                    
+                })
             case .notDetermined:
                 Text("Finding your location...")
                 ProgressView()
             case .restricted, .denied:
                 Text("Access to location data was restricted or denied.")
-            case .authorizedWhenInUse:
-                Text("Your current location is:")
-                Text("lat: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error")")
-                Text("lng: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error")")
-                Text("Distance Traveled: \(distanceTraveled)")
-                Button("Start Location Tracking", action: {
-                    
-                })
             default:
                 ProgressView()
             }
